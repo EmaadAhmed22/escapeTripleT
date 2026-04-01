222617a9841881659eaa517628e26446af67db72
Nav Shrivastava <navsh001@gmail.com>
2026-04-01 12:33:03 -0500

    Game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadGameLevel()
+    {
+        SceneManager.LoadScene("gameLevel");
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("startMenu");
+    }
+
+    public void LoadEndScreen()
+    {
+        SceneManager.LoadScene("endScreen");
+    }
+
+    public void LoadWinScreen()
+    {
+        SceneManager.LoadScene("winScreen");
+    }
+}
